d187e036fc21df40c96525e5f93d91ed0d2be1e3
Jonathan Barone <johnito.barone@gmail.com>
2020-07-30 08:38:04 +0800

    using youtube stream

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require("express");
 const http = require("http");
 const bandcamp = require('bandcamp-scraper');
-const YoutubeMp3Downloader = require("youtube-mp3-downloader");
+const YoutubeStream = require('youtube-audio-stream');
 const YoutubeSearcher = require( 'yt-search' )
 const cors = require('cors');
 const { promisify } = require('util');
@@ -66,6 +66,15 @@ app.post('/youtube/searchById', (req, res) => {
     } )
 });
 
+app.get('/youtube/stream', (req, res) => {
+    const requestUrl = `http://youtube.com/watch?v=${req.query.videoId}`
+    try {
+      YoutubeStream(requestUrl).pipe(res)
+    } catch (exception) {
+      res.status(500).send(exception)
+    }
+});
+
 app.post('/cleanup', (req, res) => {
     // Check api key
     if ( 'key' in req.body && req.body.key == "gLCZU4KfPBmxMNQm") {
@@ -96,43 +105,43 @@ app.post('/cleanup', (req, res) => {
 });
 
 // create socket with client who ordered download
-io.on("connection", (socket) => {
-  console.log("New client connected");
-
-  socket.on("youtube/download", (videoId) => {
-      const YD = new YoutubeMp3Downloader({
-        "ffmpegPath": "ffmpeg",        // Where is the FFmpeg binary located?
-        "outputPath": __dirname + "/public/yt",    // Where should the downloaded and encoded files be stored?
-        "youtubeVideoQuality": "highest",       // What video quality should be used?
-        "queueParallelism": 2,                  // How many parallel downloads/encodes should be started?
-        "progressTimeout": 2000                 // How long should be the interval of the progress reports
-      });
-      console.log('Starting download of', videoId)
-      YD.on("finished", function(err, data) {
-          console.log('Finished downloading')
-          socket.emit('done', JSON.stringify(data));
-      });
-
-      YD.on("error", function(error) {
-          console.log('ERROR', error)
-          socket.emit('error', JSON.stringify(error));
-      });
-
-      YD.on("progress", function(progress) {
-          console.log('Progress', progress)
-          socket.emit('progress', JSON.stringify(progress));
-      });
-
-      process.on('uncaughtException', function (err) {
-          console.log('UNCAUGHT    EXCEPTION - keeping process alive:', err);
-      });
-
-      YD.download(videoId, videoId + '.mp3');
-  })
-  socket.on('disconnect', () => {
-    console.log('client disconnected');
-  });
-
-});
+// io.on("connection", (socket) => {
+//   console.log("New client connected");
+//
+//   socket.on("youtube/download", (videoId) => {
+//       const YD = new YoutubeMp3Downloader({
+//         "ffmpegPath": "ffmpeg",        // Where is the FFmpeg binary located?
+//         "outputPath": __dirname + "/public/yt",    // Where should the downloaded and encoded files be stored?
+//         "youtubeVideoQuality": "highest",       // What video quality should be used?
+//         "queueParallelism": 2,                  // How many parallel downloads/encodes should be started?
+//         "progressTimeout": 2000                 // How long should be the interval of the progress reports
+//       });
+//       console.log('Starting download of', videoId)
+//       YD.on("finished", function(err, data) {
+//           console.log('Finished downloading')
+//           socket.emit('done', JSON.stringify(data));
+//       });
+//
+//       YD.on("error", function(error) {
+//           console.log('ERROR', error)
+//           socket.emit('error', JSON.stringify(error));
+//       });
+//
+//       YD.on("progress", function(progress) {
+//           console.log('Progress', progress)
+//           socket.emit('progress', JSON.stringify(progress));
+//       });
+//
+//       process.on('uncaughtException', function (err) {
+//           console.log('UNCAUGHT    EXCEPTION - keeping process alive:', err);
+//       });
+//
+//       YD.download(videoId, videoId + '.mp3');
+//   })
+//   socket.on('disconnect', () => {
+//     console.log('client disconnected');
+//   });
+//
+// });
 
 server.listen(process.env.PORT || 5000, () => console.log(`Example app listening at http://localhost:5000`))
